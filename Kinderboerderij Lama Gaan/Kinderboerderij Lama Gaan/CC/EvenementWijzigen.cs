1325aa6d5c96ee143149e813461b2b03001bf02b
Mitch Bergmans <mitchbergmans@hotmail.com>
2015-06-30 22:10:58 +0200

    Inschrijving proberen te maken

---
+++ Kinderboerderij Lama Gaan/Kinderboerderij Lama Gaan/CC/EvenementWijzigen.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Common.CommandTrees;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Kinderboerderij_Lama_Gaan.About;
+using LamaGaan.BU;
+
+
+namespace Kinderboerderij_Lama_Gaan.CC
+{
+    class EvenementWijzigen
+    {
+
+        public string WijzigEvenement(Dictionary<String, object> evenement)
+        {
+            string antwoord;
+
+            try
+            {
+                string naam = evenement["naam"].ToString();
+                string beschrijving = evenement["beschrijving"].ToString();
+                
+
+
+
+                LamaGaan.BU.Evenementen Evenement = new LamaGaan.BU.Evenementen()
+                {
+                    naam = naam,
+                    beschrijving = beschrijving,
+                };
+                LamaGaan.BU.Evenementen.Aanmaken(Evenement);
+                antwoord = "gelukt";
+            }
+            catch (Exception)
+            {
+                antwoord = "Mislukt";
+            }
+            return antwoord;
+        }
+    }
+}
+
